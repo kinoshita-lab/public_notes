0b71ebc68124734073775a88a74b15a26f8c8828
Kaz Saita(WSL2) <saita@kinoshita-lab.org>
2024-02-12 23:20:09 +0900

    Revert "commit(auto)"
    This reverts commit 752ef9c5e067900f0ac27fe6f1f37a2b5dcd5080.

---
+++ quartz.config.ts
@@ -0,0 +1,82 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "kazbo public notes",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "google",
+      tagId: "G-JVXFC0DCRE",
+    },
+    locale: "en-US",
+    baseUrl: "kinoshita-lab.github.io/public_notes",
+    ignorePatterns: ["private", "templates", ".obsidian", "obsidian_templates_"],
+    defaultDateType: "modified",
+    theme: {
+      cdnCaching: true,
+      typography: {
+        header: "Noto Serif JP",
+        body: "Noto Serif JP",
+        code: "Noto Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#faf8f8",
+          lightgray: "#e5e5e5",
+          gray: "#000000",
+          darkgray: "#000000",
+          dark: "#000000",
+          secondary: "#284b63",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+        },
+        darkMode: {
+          light: "#161618",
+          lightgray: "#393639",
+          gray: "#ffffff",
+          darkgray: "#ffffff",
+          dark: "#ffffff",
+          secondary: "#7b97aa",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.TableOfContents(),
+      Plugin.CreatedModifiedDate({
+        // you can add 'git' here for last modified from Git
+        // if you do rely on git for dates, ensure defaultDateType is 'modified'
+        priority: ["git"],
+      }),
+      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.SyntaxHighlighting(),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources({ fontOrigin: "googleFonts" }),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: false,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
